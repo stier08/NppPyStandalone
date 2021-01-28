49d763481e8f61f28f9f78248db52c9c1abbb93d
stier08 <stier08@gmail.com>
2021-01-28 08:09:43 +0100

    getStringFromObject - unicode suppport

--- NppScintillaPython/src/ScintillaWrapper.cpp
+++ NppScintillaPython/src/ScintillaWrapper.cpp
@@ -54,9 +54,13 @@ std::string ScintillaWrapper::getStringFromObject(boost::python::object o)
     std::string raw;
     if (PyUnicode_Check(o.ptr()))
 	{
-        boost::python::object utf8Text = o.attr("encode")("utf-8");
-        raw = std::string(boost::python::extract<const char *>(utf8Text), _len(utf8Text));
-	} 
+        boost::python::object utf8Text = o.attr("__str__")();
+        raw = std::string(boost::python::extract<const char *>(utf8Text));
+	}
+	else if (PyBytes_CheckExact(o.ptr()))
+	{
+		raw.assign(PyBytes_AsString(o.ptr()), PyBytes_Size(o.ptr()));
+	}
 	else
 	{
         boost::python::object rawString = o.attr("__str__")();
